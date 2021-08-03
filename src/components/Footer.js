b55251d3e73ec6a34681ebd5b7192f65f0647d9c
lillie <lillie@Lillies-MacBook-Pro.local>
2021-08-03 16:35:42 -0700

    updated Navbar js & css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,10 @@ import React from "react"
 
 export default function Footer(){
     return(
+        <footer>
         <div className="rounded-lg text-center text-brown bg-gray-800" >
             <h6 href="">&copy; Copyright 2021, LillieMefie</h6>
         </div>
+        </footer>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,31 +4,27 @@ import '../style/Navbar.css';
 
 export default function Navbar () {
   return (
-    <header>
+<header>
 <nav className="navbar navbar-expand-lg navbar-light bg-white">
   <div className="container-fluid">
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <NavLink to="/" className="nav-link active" aria-current="page" href="#">Home</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/about" className="nav-link">About me</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/project" className="nav-link">Project</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/contact" className="nav-link">Contact</NavLink>
-        </li>
-        <li className="nav-item">
-          <a href="#" className="nav-link">Resume</a>
-        </li>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <NavLink to="/" className="navbar-brand"></NavLink>
+    <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+      <ul className="navbar-nav mb-2 mb-lg-0">
+        <li className="nav-item"><NavLink to="/"  className="nav-link active" aria-current="page">HOME</NavLink></li>
+        <li className="nav-item"><NavLink to="/about" className="nav-link">ABOUT</NavLink></li>
+        <li className="nav-item"><NavLink to="/project" className="nav-link">PROJECT</NavLink></li>
+        <li className="nav-item"><NavLink to="/contact" className="nav-link">CONTACT</NavLink></li>
+        <li className="nav-item"> <a className="nav-link" href="https://drive.google.com/file/d/1rVZOqBVKmUdHKdNEUxb2TzAyHa2rOQI3/view?usp=sharing"
+         target="_blank">RESUME</a></li>
       </ul>
     </div>
   </div>
 </nav>
 </header>
+
   )
 };
 
