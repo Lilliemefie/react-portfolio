4d035d530c4abfa1f1c2256c3bc8f200879893ed
lillie <lillie@Lillies-MacBook-Pro.local>
2021-08-03 18:26:50 -0700

    added imgs and created Project js & css

---
+++ src/pages/Project.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import '../style/Project.css'
+
+
+export default function Project (){
+  return(
+<div class="container text-center">
+  <h1>Projects</h1>
+  <p><em>I do what I love!</em></p>
+  <h4>What I have created...</h4>
+  <br />
+  <div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <div class="btnstyle">
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <a href="#" class="btn btn-success">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <div class="btnstyle">
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <a href="#" class="btn btn-success">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <div class="btnstyle">
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <a href="#" class="btn btn-success">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <div class="btnstyle">
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <a href="#" class="btn btn-success">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <div class="btnstyle">
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <a href="#" class="btn btn-success">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <div class="btnstyle">
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <a href="#" class="btn btn-success">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+  )
+
+
+
+
+}
