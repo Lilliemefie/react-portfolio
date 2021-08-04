415630a9d8ef06c3aab197320e466083ef93ef4a
lillie <lillie@Lillies-MacBook-Pro.local>
2021-08-04 12:02:53 -0700

    updated img size Project.css and style Home.css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,9 @@ import '../style/Home.css'
 export default function Home () {
     return (
         <div className="condiv home">
+            <div className="col-m-8 ">
             <ReactTypingEffect text={['I am Lillie Jirapattanalak', 'I am a Web Developer']} speed={80} eraseDelay={200} className="typingeffect" />
+            </div>
             <img src="https://github.com/Lilliemefie/react-portfolio/blob/main/src/assets/lillie%20avatar.PNG?raw=true" alt="profile" className="profilepic" />
             <div className="row">
             <div className="col-m-8 ">
